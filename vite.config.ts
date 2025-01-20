c62993f421a3cc805582b3ebd47d2e3de5a3eb2a
zechu <zechu.miao@js.design>
2025-01-20 17:25:13 +0800

    fix: change import way

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,5 @@
-import { defineConfig, createLogger, LogOptions } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-const customLogger = createLogger();
-
-const info = customLogger.info;
-
-customLogger.info = (msg: string, options: LogOptions) => {
-    info(`🔥🔥🔥-${msg}`, options);
-}
 
 
 // https://vite.dev/config/
@@ -21,6 +13,5 @@ export default defineConfig({
     __APP_VERSION__: JSON.stringify("1.0.0"),
   },
   publicDir: "public",
-  customLogger,
   envDir: 'env'
 })
